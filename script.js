ffa9193fb332b18e5c2021a3d4d808b729cf55be
Sarah Cabral <sarah000cabral@gmail.com>
2024-07-12 18:34:13 -0300

    finished javascript and adjusted responsive css mobile

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-function removerConteudoSaida() {
-    document.getElementById('conteudo__saida').style.display = 'none';
+function mudaSaidaInicial() {
+    document.getElementById('conteudo__saida__inicial').style.display = 'none';
+    document.getElementById('conteudo__saida__dados').style.display = 'block';
 }
 
-function pegaMensagem(){
+function recebeMensagemInicial(){
     document.getElementById('botao__criptografar').addEventListener("click",criptografia);
     document.getElementById('botao__descriptografar').addEventListener("click",descriptografia);
 }
 
-pegaMensagem();
+function mostraMensagemModificada(mensagemModificada){
+    let campo__mensagemModificada = document.querySelector('.saida__dados__ajuste__texto');
+    return campo__mensagemModificada.innerHTML = mensagemModificada;
+}
+
+recebeMensagemInicial();
 
 function criptografia(){
+    mudaSaidaInicial();
+
     let texto = document.querySelector('.conteudo__entrada__campo__input').value;
-    console.log('Criptografia em ação');
+    let textoCriptografado = texto.replace(/e/g,'enter')
+        .replace(/i/g,'imes')
+        .replace(/a/g,'ai')
+        .replace(/o/g,'ober')
+        .replace(/u/g,'ufat');
+
+    return mostraMensagemModificada(textoCriptografado);
 }
 
 function descriptografia(){
-    console.log('Descriptografia em ação');
+    mudaSaidaInicial();
+
+    let texto = document.querySelector('.conteudo__entrada__campo__input').value;
+
+    let textoDescriptografado = texto.replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+
+    return mostraMensagemModificada(textoDescriptografado);
 }
